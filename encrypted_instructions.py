3268c9e2b126d2301f05a6e8b1c1b22358da8185
lina1906 <angelina.fefelova190602@gmail.com>
2024-11-20 15:54:08 +0300

    Update encrypted_instructions.py

--- encrypted_instructions.py
+++ encrypted_instructions.py
@@ -1,29 +1,27 @@
-#125793073
+#125805345
 valid_values = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 
 
 def decode_string(encoded_string: str) -> str:
     """Расшифровывает строку."""
-    result = ""
     stack = []
-    count = ""
-    current_fragment = ""
+    count = ''
+    current_fragment = ''
 
     for char in encoded_string:
         if char in valid_values:
             count += char
         elif char == '[':
             stack.append((current_fragment, count))
-            current_fragment = ""
-            count = ""
+            current_fragment = ''
+            count = ''
         elif char == ']':
-            last, repeat_count = stack.pop()
-            current_fragment = last + current_fragment * int(repeat_count)
+            previous_fragment, repeat_count = stack.pop()
+            current_fragment = previous_fragment + current_fragment * int(repeat_count)
         else:
             current_fragment += char
 
-    result += current_fragment
-    return result
+    return current_fragment
 
 
 if __name__ == "__main__":
